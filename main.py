319b5460dd2058ec8622b227bca801466fbd47fe
gidleyw <gidleyw@gmail.com>
2017-01-29 22:16:49 -0600

    video 2/6 done

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = 'Hellloooo world!'
         encrpyted_message = caesar.encrypt(message, 13)
-        self.response.write(encrpyted_message)
+        textarea = "<textarea name='text' style = 'height: 100px; width; 400px'>" + encrpyted_message + "</textarea>"
+        submit = "<input type = 'submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
